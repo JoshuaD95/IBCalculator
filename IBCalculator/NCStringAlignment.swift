55f44815c7f41cb04b9c883f91f6497a624c7702
SuperSneed <51832457+SuperSneed@users.noreply.github.com>
2019-06-20 13:03:37 -0400

    update Swift3.0

---
+++ IBCalculator/NCStringAlignment.swift
@@ -0,0 +1,18 @@
+//
+//  NCStringAlignment.swift
+//  IBCalculator
+//
+import Foundation
+
+/// The alignment for drawing an String inside a bounding rectangle.
+enum NCStringAlignment {
+  case leftTop
+  case centerTop
+  case rightTop
+  case leftCenter
+  case center
+  case rightCenter
+  case leftBottom
+  case centerBottom
+  case rightBottom
+}
